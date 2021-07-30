6cd3c5b8228f71c07460d5a68afa43081a2b5b28
JustAnIdi0t <eliarcherarenas@gmail.com>
2021-07-30 17:13:24 -0600

    ()

--- Hello_world.cc
+++ Hello_world.cc
@@ -1,7 +1,12 @@
-#include <iostream.h>
+#include <iostream> //This is what we must include to print something to screen, or to take in an input from the user
+
+//Everything that we run in this program must be run in the int main() function
 
 int main()
 {
-	printf("Hello, world");
-	return 0;
-}
+	std::cout << "Hello, world"; //This is the text that will be printed out
+
+	return 0; //0 must be returned if int main to know that everything ran
+} 
+
+//Every piece of the code must be inside of the 2 curly brackets, this is called a code block
