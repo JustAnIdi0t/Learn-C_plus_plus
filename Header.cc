d6be1be74b427d95d3886dfb021810292e498dbf
JustAnIdi0t <eliarcherarenas@gmail.com>
2021-07-30 18:10:51 -0600

    ()

---
+++ Header.cc
@@ -0,0 +1,32 @@
+// This will introduce headers
+//
+// Headers are imported at the beginning of a script, and can be used to access special keywords
+//
+// for example
+//
+#include <iostream>
+
+//A header must start with a # to tell the computer that it is about to use methods from another source
+//
+//the include is used as a segway to the header
+//
+//the <> have to be around a header
+//
+//iostream stands for input output stream
+//
+//This just means that we can use it to take input from a user using the keyword cin, and output something using the keyword cout
+//
+//for example
+//
+int main()
+{
+	std::cout << "Hello there";
+	std::cin >> "hello there";
+	return 0;
+}
+
+//One takes in an input, and one puts one out
+//
+//the >> and << are used with cout and cin, always
+//
+//7/30/2021
